74a5e32d60f259544858ad30ef852776b5da08f2
thecoshman <thecoshman@gmail.com>
2013-06-17 21:19:31 +0100

    Made buffer calss more flexible Can handle different 'modes' of functioning Still to work out how exactly I want to pass the data in to be buffered

--- src/vertexBuffer.hpp
+++ src/vertexBuffer.hpp
@@ -1,7 +1,20 @@
 #pragma once
 namespace gldr{
     struct VertexBuffer{
-        VertexBuffer(){
+        enum class BufferType : GLuint{
+            DATA  = gl::GL_ARRAY_BUFFER,
+            INDEX = gl::GL_ELEMENT_ARRAY_BUFFER
+        };
+
+        enum class Usage : GLuint{
+            STATIC_DRAW  = gl::GL_STATIC_DRAW,
+            DYNAMIC_DRAW = gl::GL_DYNAMIC_DRAW
+            /* STATIC_READ, STATIC_WRITE, DYNAMIC_READ, DYNAMIC_WRITE*/
+        };
+
+        VertexBuffer(BufferType bufferType = BufferType::DATA, Usage usage = Usage::STATIC_DRAW):
+            bufferType(bufferType), usage(usage)
+        {
             gl::GenBuffers(1, &vbo);
         }
 
@@ -11,14 +24,16 @@ namespace gldr{
 
         void bufferData(std::vector<GLfloat> data){
             bind();
-            gl::BufferData(gl::GL_ARRAY_BUFFER, sizeof(GLfloat) * data.size(), data.data(), gl::GL_STATIC_DRAW);
+            gl::BufferData(static_cast<GLuint>(bufferType), sizeof(GLfloat) * data.size(), data.data(), static_cast<GLuint>(usage));
         }
 
         void bind(){            
-            gl::BindBuffer(gl::GL_ARRAY_BUFFER, vbo);
+            gl::BindBuffer(static_cast<GLuint>(bufferType), vbo);
         }
 
     private:
         GLuint vbo;
+        BufferType bufferType;
+        Usage usage;
     };
 }
