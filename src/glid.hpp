7b1604d8d16273fbc108899f3080128fd20c80db
thecoshman <thecoshman@gmail.com>
2013-07-02 14:04:36 +0100

    Removed indentation for main (gldr) namespace

--- src/glid.hpp
+++ src/glid.hpp
@@ -1,28 +1,28 @@
 #pragma once
 namespace gldr{
-    template <typename T>
-    struct Glid{
-        Glid():
-            id(T::create()){}
+template <typename T>
+struct Glid{
+    Glid():
+        id(T::create()){}
+        
+    Glid(Glid&& other):
+        id(other.id){
+        other.id = 0;
+    }
 
-        Glid(Glid&& other):
-            id(other.id){
-            other.id = 0;
-        }
+    ~Glid(){
+        T::destroy(id);
+    }
 
-        ~Glid(){
-            T::destroy(id);
-        }
+    operator GLuint() const{
+        return id;
+    }
 
-        operator GLuint() const{
-            return id;
-        }
+    GLuint* ptr(){
+        return &id;
+    }
 
-        GLuint* ptr(){
-            return &id;
-        }
-
-    private:
-        GLuint id;
-    };
+private:
+    GLuint id;
+};
 }
