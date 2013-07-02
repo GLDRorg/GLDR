05cd83ec4b1ecd39d116687e155b4b46b17d191b
thecoshman <thecoshman@gmail.com>
2013-07-02 13:50:23 +0100

    Create glid.hpp

---
+++ src/glid.hpp
@@ -0,0 +1,28 @@
+#pragma once
+namespace gldr{
+    template <typename T>
+    struct Glid{
+        Glid():
+            id(T::create()){}
+
+        Glid(Glid&& other):
+            id(other.id){
+            other.id = 0;
+        }
+
+        ~Glid(){
+            T::destroy(id);
+        }
+
+        operator GLuint() const{
+            return id;
+        }
+
+        GLuint* ptr(){
+            return &id;
+        }
+
+    private:
+        GLuint id;
+    };
+}
